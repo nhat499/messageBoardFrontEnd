270e3ceb4b140fcdb352cf652372af3e9790b398
Nhat Trang <nhat499@uw.edu>
2022-08-04 16:46:17 -0700

    a

--- src/util/variables.js
+++ src/util/variables.js
@@ -3,9 +3,10 @@ const DEV_MODE = true;
 const CLIENT_URL='';
 
 
-let SERVER_URL= 'http://localhost:8000';
+//let SERVER_URL= 'http://localhost:8000';
 
 
-if (DEV_MODE) SERVER_URL= 'http://34.216.189.30/api/';
+//if (DEV_MODE) 
+const SERVER_URL= 'http://34.216.189.30/api/';
 
 export {CLIENT_URL, SERVER_URL, DEV_MODE};
